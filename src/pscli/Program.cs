22232d128e46b2729410366733be9899a1e8f484
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2024-04-16 16:09:16 +0300

    Create dotnet console app

---
+++ src/pscli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.CommandLine;
+
+namespace Pscli;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Pscli is a application for creating planned schedule(ps) data.");
+   
+        rootCommand.SetHandler(() => Console.WriteLine("Hello, World!"));
+
+        return await rootCommand.InvokeAsync(args);
+    }
+}
