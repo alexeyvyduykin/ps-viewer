92ca20d25163491bd0fcdc6a7b815b326b099240
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2024-04-22 20:50:37 +0300

    Create classlib for MongoDB entities

---
+++ shared/Shared.Entities.MongoDB/PlannedScheduleEntity.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Shared.Models;
+
+namespace Shared.Entities.MongoDB;
+
+public class PlannedScheduleEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public required string Name { get; init; }
+
+    public required DateTime DateTime { get; init; }
+
+    public required List<Satellite> Satellites { get; init; }
+
+    public required List<GroundTarget> GroundTargets { get; init; }
+
+    public required List<GroundStation> GroundStations { get; init; }
+
+    public required List<ObservationTask> ObservationTasks { get; init; }
+
+    public required List<CommunicationTask> CommunicationTasks { get; init; }
+
+    public required List<Availability> ObservationWindows { get; init; }
+
+    public required List<Availability> CommunicationWindows { get; init; }
+
+    public required List<ObservationTaskResult> ObservationTaskResults { get; init; }
+
+    public required List<CommunicationTaskResult> CommunicationTaskResults { get; init; }
+}
