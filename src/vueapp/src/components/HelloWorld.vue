62f5bb5cd0b41e831463f88845676bf745cb4958
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2024-04-25 09:46:44 +0300

    Connect to server

--- src/vueapp/src/components/HelloWorld.vue
+++ src/vueapp/src/components/HelloWorld.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
@@ -11,9 +5,29 @@ defineProps<{
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <div>{{ data }}</div>
     </h3>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+defineProps<{
+  msg: string
+}>()
+
+const data = ref()
+
+onMounted(() => {
+  fetch('api/ps/getsatellites')
+    .then((r) => r.json())
+    .then((json) => {
+      data.value = json
+      return
+    })
+})
+</script>
 
 <style scoped>
 h1 {
--- src/vueapp/vite.config.ts
+++ src/vueapp/vite.config.ts
@@ -2,17 +2,65 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+import path from 'path'
+import child_process from 'child_process'
+import { env } from 'process'
 import VueDevTools from 'vite-plugin-vue-devtools'
+
+const baseFolder =
+  env.APPDATA !== undefined && env.APPDATA !== ''
+    ? `${env.APPDATA}/ASP.NET/https`
+    : `${env.HOME}/.aspnet/https`
+
+const certificateName = 'vueappremoveafter.client'
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+  if (
+    0 !==
+    child_process.spawnSync(
+      'dotnet',
+      ['dev-certs', 'https', '--export-path', certFilePath, '--format', 'Pem', '--no-password'],
+      { stdio: 'inherit' }
+    ).status
+  ) {
+    throw new Error('Could not create certificate.')
+  }
+}
+
+const target2 = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+    ? env.ASPNETCORE_URLS.split(';')[0]
+    : 'https://localhost:7275'
+
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `http://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+    ? env.ASPNETCORE_URLS.split(';')[0]
+    : 'http://localhost:5000'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '^/api': {
+        target,
+        secure: false
+      }
+    },
+    port: 5173,
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath)
     }
   }
 })
